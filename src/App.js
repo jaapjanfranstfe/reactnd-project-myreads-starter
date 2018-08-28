19693c9eacd53cb017a1e996e5fc7ef3affe9f2d
Jaap-Jan Frans <jaapjan@gmail.com>
2018-08-28 22:41:48 +0200

    added bookshelf with book components added lifecycle hook in wich books are retrieved

--- src/App.js
+++ src/App.js
@@ -7,15 +7,38 @@ import SearchBooks from './SearchBooks'
 import './App.css'
 
 class BooksApp extends Component {
-  state = {
-    shelves: {
-        currentlyReading: [],
-        wantToRead: [],
-        read: []
-    }
-  }
+    state = {
+        shelves: []
+    };
 
-  render() {
+    componentDidMount() {
+        BooksAPI.getAll()
+            .then((books) => {
+                const current = {
+                    id: "currentlyReading",
+                    title: "Currently Reading",
+                    books: books.filter((book) => book.shelf === "currentlyReading")
+                };
+
+                const want = {
+                    id: "wantToRead",
+                    title: "Want to Read",
+                    books: books.filter((book) => book.shelf === "wantToRead")
+                };
+
+                const read = {
+                    id: "read",
+                    title: "Read",
+                    books: books.filter((book) => book.shelf === "read")
+                };
+
+                this.setState(() => ({
+                    shelves: [current, want, read]
+                }));
+            })
+    }
+
+    render() {
     return (
         <div className="app">
             <Route exact path='/' render={() => (
@@ -26,7 +49,7 @@ class BooksApp extends Component {
             )}/>
         </div>
     )
-  }
+    }
 }
 
 export default BooksApp
---
+++ src/Book.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class Book extends Component {
+
+    render() {
+        const { data } = this.props;
+        return (
+            <li>
+                <div className="book">
+                    <div className="book-top">
+                        // TODO: create separate component for book cover that also has a default placeholder if image not available
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${data.imageLinks.thumbnail}")` }}></div>
+                        <div className="book-shelf-changer">
+                            <select>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="book-title">{data.title}</div>
+                    <div className="book-authors">{data.authors.join(', ')}</div>
+                </div>
+            </li>
+        );
+    }
+}
+
+export default Book;
