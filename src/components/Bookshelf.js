ceb2e6891c42b874f6bf66dbc69b967bba6caaf4
Jaap-Jan Frans <jaapjan@gmail.com>
2018-08-31 15:41:25 +0200

    searchbar component added

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Book from './Book'
+import Book from './Book';
 import { bookPropType } from "../CustomPropTypes";
 import PropTypes from "prop-types";
 
